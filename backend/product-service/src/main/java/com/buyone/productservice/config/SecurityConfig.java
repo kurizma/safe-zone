8c03c8c08a4adca40e60be21fc13fb86d811333d
mareerray <mareerray@gmail.com>
2026-01-10 16:03:51 +0200

    Replace this use of System.out by a logger.

--- backend/product-service/src/main/java/com/buyone/productservice/config/SecurityConfig.java
+++ backend/product-service/src/main/java/com/buyone/productservice/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.buyone.productservice.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,9 +9,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        System.out.println("!!! SecurityConfig loaded, CSRF disabled !!!");
+        logger.info("!!! SecurityConfig loaded, CSRF disabled !!!");
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
