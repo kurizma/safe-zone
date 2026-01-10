91f7222ab5b033cd6c95fa06cdf3c77243606dae
mareerray <mareerray@gmail.com>
2026-01-10 17:30:35 +0200

    Make UserServiceApplicationTests use empty context (no Mongo/Auth)

--- backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
+++ backend/user-service/src/test/java/com/buyone/userservice/UserServiceApplicationTests.java
@@ -1,11 +1,11 @@
 package com.buyone.userservice;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = {})
+@ContextConfiguration(classes = {}) 
 class UserServiceApplicationTests {
 
 	@Test
